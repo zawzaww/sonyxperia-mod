79488ce29ac1437a05691fe80945b1fc9d625298
zawzaw-playerz <mczawzaw12@outlook.com>
2017-08-11 20:31:29 +0630

    Config: add system file lists and permissions

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=purezmod
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -48,7 +48,10 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "       PureZ Xperia MOD        "
+  ui_print "                               "
+  ui_print "              By               "
+  ui_print "            ZawZaw             "
   ui_print "*******************************"
 }
 
@@ -72,6 +75,45 @@ REPLACE="
 # Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/addon.d/purezxperia.sh
+/system/app/SemcHome/XperiaHome.apk
+/system/app/SemcIconPack/XperiaXIcon.apk
+/system/etc/permissions/com.sony.device.xml
+/system/etc/permissions/com.sonyericsson.colorextraction.xml
+/system/etc/permissions/com.sonyericsson.dlna.xml
+/system/etc/permissions/com.sonyericsson.dtcpctrl_private.xml
+/system/etc/permissions/com.sonyericsson.dtcpctrl_protected.xml
+/system/etc/permissions/com.sonyericsson.psm.sysmonservice.thermal.xml
+/system/etc/permissions/com.sonyericsson.system.xml
+/system/etc/permissions/com.sonyericsson.updatecenter.xml
+/system/etc/permissions/com.sonyericsson.uxpres.xml
+/system/etc/permissions/com.sonyericsson.video.player.service.xml
+/system/etc/permissions/com.sonyericsson.video.settings.xml
+/system/etc/permissions/com.sonymobile.home.resourceprovider.xml
+/system/etc/permissions/com.sonymobile.imageenhancer.xml
+/system/etc/permissions/com.sonymobile.playanywhere.xml
+/system/etc/permissions/com.sonymobile.product.xml
+/system/etc/permissions/com.sonymobile.sonyaudioeffect.xml
+/system/framework/com.sony.device.jar
+/system/framework/com.sonyericsson.colorextraction_impl.jar
+/system/framework/com.sonyericsson.dlna_impl.jar
+/system/framework/com.sonyericsson.dtcpctrl_private_impl.jar
+/system/framework/com.sonyericsson.dtcpctrl_protected_impl.jar
+/system/framework/com.sonyericsson.psm.sysmonservice.thermal_impl.jar
+/system/framework/com.sonyericsson.system.jar
+/system/framework/com.sonyericsson.updatecenter.jar
+/system/framework/com.sonyericsson.uxp.jar
+/system/framework/com.sonyericsson.uxpres.jar
+/system/framework/com.sonyericsson.video.jar
+/system/framework/com.sonyericsson.video.player.service.jar
+/system/framework/com.sonyericsson.video.settings.jar
+/system/framework/com.sonymobile.album.jar
+/system/framework/com.sonymobile.home.resourceprovider.jar
+/system/framework/com.sonymobile.imageenhancer_impl.jar
+/system/framework/com.sonymobile.photoeditor.jar
+/system/framework/com.sonymobile.playanywhere.jar
+/system/framework/com.sonymobile.product_impl.jar
+/system/framework/sonyaudioeffect.jar
 "
 
 ##########################################################################################
@@ -83,6 +125,14 @@ REPLACE="
 set_permissions() {
   # Default permissions, don't remove them
   set_perm_recursive  $MODPATH  0  0  0755  0644
+
+  # set_perm_recursive_dir
+  set_perm_recursive  $MODPATH/system/app              0  0  0755  0644
+  set_perm_recursive  $MODPATH/system/etc/permissions  0  0  0755  0644
+  set_perm_recursive  $MODPATH/system/framework        0  0  0755  0644
+
+  # set_perm_file
+  set_perm  $MODPATH/system/addon.d/purezxperia.sh  0  0  0755
 
   # Only some special files require specific permissions
   # The default permissions should be good enough for most cases
