4b89f734ef99a6bb3cf48519b600a9400acf3d3d
zawzaw-playerz <mczawzaw12@outlook.com>
2017-08-12 07:04:19 +0630

    config: redesigned ui_print

--- config.sh
+++ config.sh
@@ -47,12 +47,15 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "       PureZ Xperia MOD        "
-  ui_print "                               "
-  ui_print "              By               "
-  ui_print "            ZawZaw             "
-  ui_print "*******************************"
+  ui_print "******************************************"
+  ui_print "***  PureZ Xperia MOD for all Devices  ***"
+  ui_print "***     ==========================     ***"
+  ui_print "***      Magisk Moudle/Flashable       ***"
+  ui_print "***                                    ***"
+  ui_print "***                 By                 ***"
+  ui_print "***       zawzaw @xda-developers       ***"
+  ui_print "***                                    ***"
+  ui_print "******************************************"
 }
 
 ##########################################################################################
